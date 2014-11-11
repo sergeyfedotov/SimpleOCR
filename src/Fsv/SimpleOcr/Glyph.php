aa5cd1ffec6a1fff87a4e64e6b7607a0b888c16f
Sergey Fedotov <sergey89@gmail.com>
2014-11-12 00:24:14 +0300

    Glyph

---
+++ src/Fsv/SimpleOcr/Glyph.php
@@ -0,0 +1,67 @@
+<?php
+namespace Fsv\SimpleOcr;
+
+/**
+ * Class Glyph
+ * @package Fsv\SimpleOcr
+ */
+class Glyph
+{
+    /**
+     * @var Bitmap
+     */
+    protected $bitmap;
+
+    /**
+     * @var string
+     */
+    protected $character;
+
+    /**
+     * @param Bitmap $bitmap
+     * @param string $character
+     */
+    public function __construct(Bitmap $bitmap, $character = '')
+    {
+        $this->bitmap = $bitmap;
+        $this->character = $character;
+    }
+
+    /**
+     * @param Bitmap $bitmap
+     * @return Glyph
+     */
+    public function setBitmap(Bitmap $bitmap)
+    {
+        $this->bitmap = $bitmap;
+
+        return $this;
+    }
+
+    /**
+     * @return Bitmap
+     */
+    public function getBitmap()
+    {
+        return $this->bitmap;
+    }
+
+    /**
+     * @param $character
+     * @return Glyph
+     */
+    public function setCharacter($character)
+    {
+        $this->character = $character;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCharacter()
+    {
+        return $this->character;
+    }
+}
